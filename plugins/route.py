3dfb49aa9367fdc365b83009aa41e24f75bf53f6
KALI <FionaWilson520866@hotmail.com>
2024-10-05 21:01:09 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,9 +1,3 @@
-#(©)Codexbotz
-#rymme
-
-
-
-
 
 from aiohttp import web
 
@@ -11,4 +5,10 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("CodeXBotz")
+    return web.json_response("BOOT ON HAI ASHU")
+
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
